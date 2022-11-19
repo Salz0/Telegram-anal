163a07c3a0afbb1a960abd03d075b49ff7928fb9
Ronnieviper <vladozeransky2k@gmail.com>
2022-11-19 23:15:32 +0200

    1. Began writing most basic tests 2. Added sample_chat.json

---
+++ tests/test_analisis.py
@@ -0,0 +1,18 @@
+import os
+from collections import Counter
+
+from telenal.analisis import who_breaks_silence_json, wordcloud_json
+
+
+def test_who_breaks_silence_json():
+    assert who_breaks_silence_json("D:/Telegram-anal/Real_life_jsons/yana.json") == Counter({'Яна': 25, 'Влад': 28})
+
+
+def test_wordcloud_json():
+    here = os.getcwd().split("Telegram-anal")[0].replace("\\", "/")
+
+    wordcloud_ = wordcloud_json(f"{here}Telegram-anal/Real_life_jsons/sample_chat.json", language="english", plot=False)
+    assert wordcloud_.words_['fund'] == 0.6559139784946236
+
+    # TODO: write an error where an html chat is input
+    # assert wordcloud_json("/Real_life_jsons/sample_chat.json", language="english", plot=False) ==
